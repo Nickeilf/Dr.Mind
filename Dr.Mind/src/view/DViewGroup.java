fb5eb6a0e5d50fc7738ba8eeff2b7582cb74c685
nickeilf <haozi1996@126.com>
2016-07-08 16:38:40 +0800

    dragable

---
+++ Dr.Mind/src/view/DViewGroup.java
@@ -0,0 +1,69 @@
+package view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DViewGroup extends ViewGroup {
+
+	private float posX = this.getX();
+	private float posY = this.getY();
+	private float startX;
+	private float startY;
+
+	public DViewGroup(Context context) {
+		super(context);
+
+	}
+
+	public DViewGroup(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public DViewGroup(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	/**
+	 * 添加View的方法
+	 */
+	public void myAddView() {
+		TextView mIcon = new TextView(this.getContext());
+		mIcon.setText("aaaaaaaaaa");
+		addView(mIcon);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			startX = event.getX();
+			startY = event.getY();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			float stopX = event.getX();
+			float stopY = event.getY();
+			Log.e("TAG", "onTouchEvent-ACTION_MOVE\nstartX is " + startX + " startY is " + startY + " stopX is " + stopX
+					+ " stopY is " + stopY);
+			posX += stopX - startX;
+			posY += stopY - startY;
+			this.setX(posX);
+			this.setY(posY);
+			invalidate();// call onDraw()
+			break;
+		}
+		return true;
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+	}
+
+}
