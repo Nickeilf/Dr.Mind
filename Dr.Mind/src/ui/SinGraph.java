fead2577c807c5e3524f8ad7dea335d72a66af33
lxing14 <141250079@smail.nju.edu.cn>
2016-07-08 12:03:04 +0800

    morning

---
+++ Dr.Mind/src/ui/SinGraph.java
@@ -0,0 +1,26 @@
+package ui;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/*
+ * @auther:Liu 
+ * @date:2016.7.8
+ * @description:画出连接节点的曲线：sin为主体，后接一段直线
+ */
+
+public class SinGraph {
+
+	private Paint paint;
+	private Canvas canvas;
+	
+	public SinGraph(Canvas c){
+		paint=new Paint();
+		this.canvas=c;
+	}
+	
+	public void drawSinGraph(){
+		
+	}
+	
+}
