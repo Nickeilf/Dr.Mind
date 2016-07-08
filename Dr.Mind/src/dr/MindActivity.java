2ad3180f32c2266a278fb1de61aee6b40a0b263e
lxing14 <141250079@smail.nju.edu.cn>
2016-07-08 10:58:38 +0800

    init the project

---
+++ Dr.Mind/src/dr/MindActivity.java
@@ -0,0 +1,14 @@
+package dr;
+
+import cn.edu.cn.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MindActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
