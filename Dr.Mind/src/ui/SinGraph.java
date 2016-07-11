a083065e2730f1e9ee1c4759315666ebcab1e416
Lemonstars <141250079@smail.nju.edu.cn>
2016-07-11 11:45:18 +0800

    middle commit

--- Dr.Mind/src/ui/SinGraph.java
+++ Dr.Mind/src/ui/SinGraph.java
@@ -65,55 +65,61 @@ public class SinGraph extends View {
 
 	private void mydraw(Canvas canvas) {
 
-//		int type=sum%2;
-//		int T=180;//四分之一周期,角度制
-//		float x_start=100;//起始像素点
-//
-//		switch (type){
-//			case 0:
-//				System.out.println("case 0");
-//				for(int i=1;i<=sum;i++){
-//					float y_value;
-//					float Ai=(float)(Math.tan(i*Math.PI/4/(sum/2.0+1))*T/2.0);
-//					System.out.println("Ai="+Ai);
-//					for(int j=0;j<T;j++){
-//						y_value=(float)(Ai*Math.sin(Math.PI*T/180*Math.PI*j/180)+50);
-//						System.out.println("Math.sin(Math.PI*T/180*Math.PI*j/180)="+Math.sin(Math.PI*T/180*Math.PI*j/180));
-//						canvas.drawPoint(j, y_value, paint);
-//					}
-//				}
-//				break;
-//			case 1:
-//			default:break;
-//		}
+		int type=sum%2;
+		double T=10*Math.PI;//二分之一周期,弧度制
+		float x_start=100;//起始像素点
+		float y_start=100;
 
+		switch (type){
+			case 0:
+				for(int i=1;i<=sum;i++){
+					    float x_value;
+						float y_value;
 
-		int height = getHeight();
-		int width = getWidth();
-		float x = 0;
-		float y1 = 0;
-		float y2 = 0;
-		float y3 = 0;
-		float y4 = 0;
-		for (int i = 0; i < 180; i++) {
-			x = i;
-			double sinValue = Math.sin((x + 90) / 180.0 * Math.PI);
-			y1 = (float) (60 * sinValue + height / 2);
-			y2 = (float) (40 * sinValue + height / 2);
-			y3 = (float) (20 * sinValue + height / 2);
-			y4 = (float) (0 * sinValue + height / 2);
-			canvas.drawPoint(x, y1, paint);
-			y2 += 20;
-			y3 += 40;
-			y4 += 60;
-			canvas.drawPoint(x, y2, paint);
-			canvas.drawPoint(x, y3, paint);
-			canvas.drawPoint(x, y4, paint);
+					    //y=Asin(wx+b)+c中的A
+					    double theta=Math.PI/2*i/(sum/2+1);
+						float Ai = (float) (T/2*Math.tan(theta));
+					    System.out.println("Ai="+Ai);
+
+						for (int j = 0; j < 180; j++) {
+							x_value=x_start+j;
+							double w=Math.PI/T;
+							y_value = (float) (Ai * Math.sin(w*Math.PI*j/180) + y_start);
+							canvas.drawPoint(x_value, y_value, paint);
+						}
+				}
+				break;
+			case 1:
+			default:break;
 		}
-		canvas.drawLine(x, y1, width, y1, paint);
-		canvas.drawLine(x, y2, width, y2, paint);
-		canvas.drawLine(x, y3, width, y3, paint);
-		canvas.drawLine(x, y4, width, y4, paint);
+
+
+//		int height = getHeight();
+//		int width = getWidth();
+//		float x = 0;
+//		float y1 = 0;
+//		float y2 = 0;
+//		float y3 = 0;
+//		float y4 = 0;
+//		for (int i = 0; i < 180; i++) {
+//			x = i;
+//			double sinValue = Math.sin((x + 90) / 180.0 * Math.PI);
+//			y1 = (float) (60 * sinValue + height / 2);
+//			y2 = (float) (40 * sinValue + height / 2);
+//			y3 = (float) (20 * sinValue + height / 2);
+//			y4 = (float) (0 * sinValue + height / 2);
+//			canvas.drawPoint(x, y1, paint);
+//			y2 += 20;
+//			y3 += 40;
+//			y4 += 60;
+//			canvas.drawPoint(x, y2, paint);
+//			canvas.drawPoint(x, y3, paint);
+//			canvas.drawPoint(x, y4, paint);
+//		}
+//		canvas.drawLine(x, y1, width, y1, paint);
+//		canvas.drawLine(x, y2, width, y2, paint);
+//		canvas.drawLine(x, y3, width, y3, paint);
+//		canvas.drawLine(x, y4, width, y4, paint);
 	}
  
 }
