5dfbfb65fd7852709b5a83fbdd27434a07b86600
Lemonstars <141250079@smail.nju.edu.cn>
2016-07-16 15:16:05 +0800

    Mybutton

---
+++ Dr.Mind/src/ui/MyButton.java
@@ -0,0 +1,11 @@
+package ui;
+
+import android.content.Context;
+import android.widget.Button;
+
+/**
+ * Created by liu on 2016/7/16.
+ */
+public class MyButton extends Button {
+    public MyButton(Context context){super(context);};
+}
---
+++ Dr.Mind/src/ui/ViewToPicture.java
@@ -0,0 +1,72 @@
+package ui;
+
+import android.graphics.Bitmap;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by Liu on 2016/7/11.
+ */
+public class ViewToPicture {
+
+    public void save(View v,String name){
+        this.saveMyBitmap(this.getViewBitmap(v),name);
+    }
+
+    private Bitmap getViewBitmap(View v) {
+        v.clearFocus();
+        v.setPressed(false);
+
+        boolean willNotCache = v.willNotCacheDrawing();
+        v.setWillNotCacheDrawing(false);
+
+        // Reset the drawing cache background color to fully transparent
+        // for the duration of this operation
+        int color = v.getDrawingCacheBackgroundColor();
+        v.setDrawingCacheBackgroundColor(0);
+
+        if (color != 0) {
+            v.destroyDrawingCache();
+        }
+        v.buildDrawingCache();
+        Bitmap cacheBitmap = v.getDrawingCache();
+        if (cacheBitmap == null) {
+            return null;
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(cacheBitmap);
+
+        // Restore the view
+        v.destroyDrawingCache();
+        v.setWillNotCacheDrawing(willNotCache);
+        v.setDrawingCacheBackgroundColor(color);
+
+        return bitmap;
+    }
+
+    private void saveMyBitmap(Bitmap mBitmap,String bitName)  {
+        File f = new File( "/sdcard/Note/"+bitName + ".jpg");
+        FileOutputStream fOut = null;
+        try {
+            fOut = new FileOutputStream(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+        try {
+            fOut.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
