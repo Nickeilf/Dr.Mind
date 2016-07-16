db32c22b9f488f3c329d93a37e76e95abf47d9a2
Lemonstars <141250079@smail.nju.edu.cn>
2016-07-16 15:23:35 +0800

    简单的button样式

--- Dr.Mind/src/ui/MyButton.java
+++ Dr.Mind/src/ui/MyButton.java
@@ -1,6 +1,9 @@
 package ui;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.widget.Button;
 
 /**
@@ -8,4 +11,17 @@ import android.widget.Button;
  */
 public class MyButton extends Button {
     public MyButton(Context context){super(context);};
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint=new Paint();
+        int height=this.getHeight();
+        int width=this.getWidth();
+        int paint_width=6;
+        paint.setStrokeWidth(6);
+        paint.setColor(Color.BLUE);
+        canvas.drawLine(0, height / 2, width, height / 2, paint);
+        canvas.drawLine(width/2,0,width/2,height,paint);
+    }
 }
